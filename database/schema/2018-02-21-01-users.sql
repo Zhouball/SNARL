77c9279a0b7543bcf204f18e249b28685d8b60f5
ubuntu <ubuntu@ip-10-0-2-15.openvpn>
2018-02-21 22:33:09 -0800

    Nishant: Created sql script to create users table. Created README to document mysql build and test cases

---
+++ database/schema/2018-02-21-01-users.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL,
+  `first_name` varchar(32),
+  `last_name` varchar(32),  
+  `status` ENUM('ACTIVE','PAUSED') NOT NULL,
+  `permission` ENUM('ADMIN', 'USER') NOT NULL,
+  `email` varchar(256) NOT NULL,
+  `hash_and_salt` varchar(64) NOT NULL,
+  `create_date` bigint(20) NOT NULL DEFAULT '0',
+  `update_date` bigint(20) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`username`),
+  UNIQUE KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
--- database/schema/setup_db.sh
+++ database/schema/setup_db.sh
@@ -4,3 +4,6 @@ DB_NAME="escality"
 
 # Create db named escality
 #mysql -u root -p < 2018-02-21-00-create_db.sql
+
+# Create users table
+mysql -u root -p $DB_NAME < 2018-02-21-01-users.sql
